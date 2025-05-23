530ecd45846681532579ca07f22576f2459cc532
khangsuz <mrkhang957@gmail.com>
2025-05-23 15:46:37 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Views/**/*.cshtml",
+    "./Pages/**/*.cshtml",
+    "./Views/Shared/**/*.cshtml"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
