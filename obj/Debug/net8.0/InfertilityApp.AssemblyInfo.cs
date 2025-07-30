e8fa6921a1115e81956ebf490c1c4630cc9bc97e
khangsuz <mrkhang957@gmail.com>
2025-07-30 21:19:39 +0700

    fixing_helper_error

--- obj/Debug/net8.0/InfertilityApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/InfertilityApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InfertilityApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+281aea59d6725dc526883a0dd30df71e61b498b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34bbc8fe2ba8490aaf9f06cb229ddc02ecc97284")]
 [assembly: System.Reflection.AssemblyProductAttribute("InfertilityApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InfertilityApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
